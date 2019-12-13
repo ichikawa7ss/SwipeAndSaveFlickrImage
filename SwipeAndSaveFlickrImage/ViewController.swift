aa16fe0a00ab72f6dc124fb36b3b95d6b1d8c91f
icchii <i2s7.chacha@gmail.com>
2019-12-13 13:26:23 +0900

    Initial Commit

---
+++ SwipeAndSaveFlickrImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeAndSaveFlickrImage
+//
+//  Created by 市川星磨 on 2019/12/13.
+//  Copyright © 2019 市川 星磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
