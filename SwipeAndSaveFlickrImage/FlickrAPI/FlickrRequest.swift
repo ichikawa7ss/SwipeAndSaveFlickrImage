99b611e03db684eb51b618b6003f383abbc4ad9f
icchii <i2s7.chacha@gmail.com>
2019-12-13 14:13:02 +0900

    リクエストプロトコルにリクエスト用のURL作成処理を実装

--- SwipeAndSaveFlickrImage/FlickrAPI/FlickrRequest.swift
+++ SwipeAndSaveFlickrImage/FlickrAPI/FlickrRequest.swift
@@ -20,11 +20,45 @@ protocol FlickrRequest {
     var baseURL : URL { get }
     var flickrMethod : FlickrMethod { get }
     var queryItems: [URLQueryItem] { get }
+    var extras : [URLQueryItem]? { get }
+
 }
 
 extension FlickrRequest {
     var baseURL : URL {
         return URL(string: "https://www.flickr.com/services/rest")!
     }
+    
+    var queryItems :[URLQueryItem] {
+        // 基本のクエリパラーメータ
+        var baseQueryItem: [URLQueryItem] =
+        [
+            URLQueryItem(name: "method", value: self.flickrMethod.rawValue),
+            URLQueryItem(name: "format", value: "json"),
+            URLQueryItem(name: "api_key", value: "fda2005ab7115e9f1d44f3e810a81b42"),
+            URLQueryItem(name: "nojsoncallback", value:"1"),
+        ]
+        
+        // APIの種類に応じて追加のパラメータを追加
+        if let extras = extras {
+            for extra in extras {
+                baseQueryItem.append(extra)
+            }
+        }
+        
+        return baseQueryItem
+    }
+    
+    // TODO extrasをメソッドによってかき
+    
+    // クエリパラメータを追加したリクエスト用のURLの作成
+    func buildUrl() -> URL {
+        let url = baseURL
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        components.queryItems = queryItems
+        return components.url!
+    }
+
+    
 }
 
