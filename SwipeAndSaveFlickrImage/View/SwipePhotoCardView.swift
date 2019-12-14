cae2376199674a9da80d40b923860f8f026101b5
icchii <i2s7.chacha@gmail.com>
2019-12-15 03:21:57 +0900

    Swipe用Viewの作成

---
+++ SwipeAndSaveFlickrImage/View/SwipePhotoCardView.swift
@@ -0,0 +1,300 @@
+//
+//  SwipePhotoCardView.swift
+//  SwipeAndSaveFlickrImage
+//
+//  Created by 市川星磨 on 2019/12/15.
+//  Copyright © 2019 市川 星磨. All rights reserved.
+//
+
+import UIKit
+
+
+/// Swipeでの動きに応じた動きをデリゲートパターンで実装
+protocol SwipePhotoCardViewDelegate : NSObjectProtocol{
+    /// ドラッグ開始時に実行されるアクション
+    func cardViewDidBeginDragging(_ cardView: SwipePhotoCardView)
+    /// 位置の変化が生じた際に実行されるアクション
+    func cardViewDidUpdatePosition(_ cardView: SwipePhotoCardView,  centerX: CGFloat, centerY: CGFloat)
+    /// 左側へのスワイプ動作が完了した場合に実行されるアクション
+    func cardViewDidSwipeLeftPosition(_ cardView: SwipePhotoCardView)
+    /// 右側へのスワイプ動作が完了した場合に実行されるアクション
+    func cardViewDidSwipeRightPosition(_ cardView: SwipePhotoCardView)
+    /// 元の位置に戻る動作が完了したに実行されるアクション
+    func cardViewDidReturnToOriginalPosition(_ cardView: SwipePhotoCardView)
+}
+
+class SwipePhotoCardView: UIView {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    /// ドラッグ処理開始時のViewがある位置を格納する変数
+    private var originalPoint: CGPoint = CGPoint.zero
+    
+    /// 中心位置からのX軸＆Y軸方向の位置を格納する変数
+    private var xPositionFromCenter: CGFloat = 0.0
+    private var yPositionFromCenter: CGFloat = 0.0
+
+    /// 中心位置からのX軸方向へ何パーセント移動したか（移動割合）を格納する変数
+    private var currentMoveXPercentFromCenter: CGFloat = 0.0
+    private var currentMoveYPercentFromCenter: CGFloat = 0.0
+
+    /// Viewの初期状態の傾きを決める変数(意図的に揺らぎを与えてランダムで少しずらす)
+    private var initialTransform: CGAffineTransform = .identity
+
+    /// PhotoCardSetDelegateのインスタンス宣言
+    weak var delegate: SwipePhotoCardViewDelegate?
+    
+    /// Viewの初期状態の中心点を決める変数(意図的に揺らぎを与えてランダムで少しずらす)
+    private var initialCenter: CGPoint = CGPoint(
+        x: UIScreen.main.bounds.size.width / 2,
+        y: UIScreen.main.bounds.size.height / 2
+    )
+    
+    // 実行表示時間
+    private let durationOfInitialize: TimeInterval = 0.93
+    private let durationOfStartDragging: TimeInterval = 0.26
+    private let durationOfReturnOriginal: TimeInterval = 0.26
+    private let durationOfSwipeOut: TimeInterval = 0.48
+    
+    // 透明度
+    private let startDraggingAlpha: CGFloat = 0.98
+    private let stopDraggingAlpha: CGFloat = 1.00
+
+    // カードの拡大縮小比率
+    private let maxScaleOfDragging: CGFloat = 1.00
+
+    // ドラッグ終了時にカードをリリースするポイントの割合
+    private let swipeXPosLimitRatio: CGFloat = 0.4
+    private let swipeYPosLimitRatio: CGFloat = 0.02
+    
+        // 初期化表示前の拡大縮小比
+    private let beforeInitializeScale: CGFloat = 1.00
+    private let afterInitializeScale: CGFloat  = 1.00
+    
+    /// このカスタムビューをコードで使用する際の初期化処理
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+        initContentView()
+        setupPhotoCardSetView()
+        setupPanGestureRecognizer()
+    }
+
+    /// このカスタムビューをInterfaceBuilderで使用する際の初期化処理
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initContentView()
+        setupPhotoCardSetView()
+        setupPanGestureRecognizer()
+    }
+    
+    /// コンテンツ表示用Viewの初期化処理
+    private func initContentView() {
+
+        /// コンテンツ表示用のView
+        weak var contentView: UIView!
+
+        /// 追加するcontentViewのクラス名を取得する
+        let viewClass: AnyClass = type(of: self)
+
+        /// 追加するcontentViewに関する設定をする
+        contentView = Bundle(for: viewClass)
+            .loadNibNamed(String(describing: viewClass), owner: self, options: nil)?.first as? UIView
+        contentView.autoresizingMask = autoresizingMask
+        contentView.frame = bounds
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(contentView)
+
+        /// 追加するcontentViewの制約を設定する ※上下左右へ0の制約を追加する
+        let bindings = ["view": contentView as Any]
+
+        let contentViewConstraintH = NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|[view]|",
+            options: NSLayoutConstraint.FormatOptions(rawValue: 0),
+            metrics: nil,
+            views: bindings
+        )
+        let contentViewConstraintV = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|[view]|",
+            options: NSLayoutConstraint.FormatOptions(rawValue: 0),
+            metrics: nil,
+            views: bindings
+        )
+        addConstraints(contentViewConstraintH)
+        addConstraints(contentViewConstraintV)
+
+    }
+    
+    // MARK: - Function
+
+    func setViewData(_ photo: Photo) {
+    }
+    
+    // MARK: - Private Function
+    
+    /// ドラッグが開始された際に実行される処理
+    @objc private func startDragging(_ sender: UIPanGestureRecognizer) {
+        
+        // 中心位置からのX軸＆Y軸方向の位置の値を更新する
+        xPositionFromCenter = sender.translation(in: self).x
+        yPositionFromCenter = sender.translation(in: self).y
+        
+        /// UIPangestureRecognizerの状態に応じた処理を行う
+        switch sender.state {
+            
+        /// ドラッグ開始時の処理
+        case .began:
+            
+            /// ドラッグ処理開始時のViewがある位置を取得する
+            originalPoint = CGPoint(
+                x: self.center.x - xPositionFromCenter,
+                y: self.center.y - yPositionFromCenter
+            )
+            
+            /// DelegeteメソッドのbeganDraggingを実行する
+            self.delegate?.cardViewDidBeginDragging(self)
+            
+            /// ドラッグ処理開始時のViewのアルファ値を変更する
+            UIView.animate(withDuration: durationOfStartDragging, delay: 0.0, options: [.curveEaseInOut], animations: {
+                self.alpha = self.startDraggingAlpha
+            }, completion: nil)
+            
+            break
+            
+        /// ドラッグ最中の処理
+        case .changed:
+            
+            /// 動かした位置の中心位置を取得する
+            let newCenterX = originalPoint.x + xPositionFromCenter
+            let newCenterY = originalPoint.y + yPositionFromCenter
+            
+            /// Viewの中心位置を更新して動きをつける
+            self.center = CGPoint(x: newCenterX, y: newCenterY)
+            
+            /// DelegeteメソッドのcardViewDidUpdatePositionを実行する
+            self.delegate?.cardViewDidUpdatePosition(self, centerX: newCenterX, centerY: newCenterY)
+            
+            /// 中心位置からのX軸方向へ何パーセント移動したか（移動割合）を計算する
+            currentMoveXPercentFromCenter = min(xPositionFromCenter / UIScreen.main.bounds.size.width, 1)
+            
+            /// 中心位置からのY軸方向へ何パーセント移動したか（移動割合）を計算する
+            currentMoveYPercentFromCenter = min(yPositionFromCenter / UIScreen.main.bounds.size.height, 1)
+            
+            let initialRotationAngle = atan2(initialTransform.b, initialTransform.a)
+            let whenDraggingRotationAngel = initialRotationAngle + CGFloat.pi / 10 * currentMoveXPercentFromCenter
+            let transforms = CGAffineTransform(rotationAngle: whenDraggingRotationAngel)
+            
+            /// 拡大縮小比を適用する
+            let scaleTransform: CGAffineTransform = transforms.scaledBy(x: maxScaleOfDragging, y: maxScaleOfDragging)
+            self.transform = scaleTransform
+            
+            break
+            
+        /// ドラッグ終了時の処理
+        case .ended, .cancelled:
+            
+            /// ドラッグ終了時点での速度を算出する
+            let whenEndedVelocity = sender.velocity(in: self)
+            
+            /// 移動割合のしきい値を超えていた場合には、画面外へ流れていくようにする（しきい値の範囲内の場合は元に戻る）
+            let shouldMoveToLeft  = (currentMoveXPercentFromCenter < -swipeXPosLimitRatio && abs(currentMoveYPercentFromCenter) > swipeYPosLimitRatio)
+            let shouldMoveToRight = (currentMoveXPercentFromCenter > swipeXPosLimitRatio && abs(currentMoveYPercentFromCenter) > swipeYPosLimitRatio)
+            
+            if shouldMoveToLeft {
+                moveInvisiblePosition(velocity: whenEndedVelocity, isLeft: true)
+            } else if shouldMoveToRight {
+                moveInvisiblePosition(velocity: whenEndedVelocity, isLeft: false)
+            } else {
+                moveOriginalPosition()
+            }
+            
+            /// ドラッグ開始時の座標位置の変数をリセットする
+            originalPoint = CGPoint.zero
+            xPositionFromCenter = 0.0
+            yPositionFromCenter = 0.0
+            currentMoveXPercentFromCenter = 0.0
+            currentMoveYPercentFromCenter = 0.0
+            self.alpha = 1.0
+            
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    // このViewに対する初期設定を行う
+    private func setupPhotoCardSetView() {
+        
+        // カード状のViewに関する基本的な設定 ※設定できるパラメータは全てPhotoCardDefaultSettings.swiftへ委譲している
+        self.clipsToBounds  = true
+        self.frame = CGRect(
+            origin: CGPoint.zero,
+            size: CGSize(
+                width: 350,
+                height: 600
+            )
+        )
+        
+        /// 画面の中心に配置
+        setInitialPosition()
+        
+        /// 背景のViewに関する設定を実装
+        self.layer.masksToBounds = false
+        self.layer.borderWidth   = 2.0
+        self.layer.cornerRadius  = 20.0
+    }
+    
+    /// このViewのUIPanGestureRecognizerの付与を行う
+    private func setupPanGestureRecognizer() {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.startDragging))
+        self.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    
+    private func setInitialPosition () {
+        /// Viewを初期位置に配置
+        self.center = self.initialCenter
+        self.transform = self.initialTransform
+    }
+    
+    /// カードを元の位置へ戻す
+    private func moveOriginalPosition() {
+        
+        UIView.animate(withDuration: durationOfReturnOriginal, delay: 0.0, usingSpringWithDamping: 0.68, initialSpringVelocity: 0.0, options: [.curveEaseInOut], animations: {
+            
+            /// Viewを初期位置に配置
+            self.setInitialPosition()
+            
+        }, completion: nil)
+        
+        /// DelegeteメソッドのreturnToOriginalPositionを実行する
+        self.delegate?.cardViewDidReturnToOriginalPosition(self)
+        
+    }
+    
+    /// カードを左側の領域外へ動かす
+    private func moveInvisiblePosition(velocity: CGPoint, isLeft: Bool = true) {
+        
+        /// 変化後の予定位置を算出する（Y軸方向の位置はvelocityに基づいた値を採用する）
+        let absPosX = UIScreen.main.bounds.size.width * 1.6
+        let endCenterPosX = isLeft ? -absPosX : absPosX
+        let endCenterPosY = velocity.y
+        let endCenterPosition = CGPoint(x: endCenterPosX, y: endCenterPosY)
+        
+        UIView.animate(withDuration: durationOfSwipeOut, delay: 0.0, usingSpringWithDamping: 0.68, initialSpringVelocity: 0.0, options: [.curveEaseInOut], animations: {
+            
+            /// ドラッグ処理終了時はViewのアルファ値を元に戻す
+            self.alpha = self.stopDraggingAlpha
+            
+            /// 変化後の予定位置までViewを移動する
+            self.center = endCenterPosition
+            
+        }, completion: { _ in
+            
+            /// DelegeteメソッドのswipedLeftPositionを実行する
+            let _ = isLeft ? self.delegate?.cardViewDidSwipeLeftPosition(self) : self.delegate?.cardViewDidSwipeRightPosition(self)
+            /// 画面から該当のViewを削除する
+            self.removeFromSuperview()
+        })
+    }
+}
---
+++ SwipeAndSaveFlickrImage/View/SwipePhotoViewController.swift
@@ -0,0 +1,249 @@
+//
+//  SwipePhotoViewController.swift
+//  SwipeAndSaveFlickrImage
+//
+//  Created by 市川星磨 on 2019/12/15.
+//  Copyright © 2019 市川 星磨. All rights reserved.
+//
+
+import UIKit
+
+final class SwipePhotoViewController: UIViewController {
+
+    public var photos: [Photo]?
+    
+    // カード表示用のViewを格納するための配列
+    fileprivate var swipePhotoList: [SwipePhotoCardView] = []
+    
+    private var currentLastPhotoNum = 0
+    
+    public var selectedPhoto:(Photo,Int)?
+
+    // 一度に追加するカード枚数の上限値
+    fileprivate let photoCardSetViewCountLimit: Int = 10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Fileprivate Function
+    
+    /// 画面上にカードを追加する
+    fileprivate func addPhotoCardSetViews(bounds: (Int,Int)) {
+
+        let min = bounds.0
+        var max = bounds.1
+        /// 取得画像数以上に読み込もうとした場合、取得できる上限を個別に設定
+        if bounds.1 >= self.photos!.count {
+            max = self.photos!.count
+        }
+        
+        for index in min ..< max {
+
+            /// photoCardSetViewのインスタンスを作成してプロトコル宣言やタッチイベント等の初期設定を行う
+            let photoCardSetView = SwipePhotoCardView()
+            photoCardSetView.delegate = (self as SwipePhotoCardViewDelegate)
+            photoCardSetView.setViewData(self.photos![index])
+            photoCardSetView.isUserInteractionEnabled = false
+            self.swipePhotoList.append(photoCardSetView)
+            
+            /// 現在表示されているカードの背面へ新たに作成したカードを追加する
+            self.view.addSubview(photoCardSetView)
+            self.view.sendSubviewToBack(photoCardSetView)
+        }
+
+        /// swipePhotoListに格納されているViewのうち、先頭にあるViewのみを操作可能にする
+        enableUserInteractionToFirstCardSetView()
+        
+        /// 画面上にあるカードの山の拡大縮小比を調節する
+        changeScaleToCardSetViews(skipSelectedView: false)
+    }
+
+    /// 画面上にあるカードの山のうち、一番上にあるViewのみを操作できるようにする
+    fileprivate func enableUserInteractionToFirstCardSetView() {
+        if !swipePhotoList.isEmpty {
+            if let firstPhotoCardSetView = swipePhotoList.first {
+                firstPhotoCardSetView.isUserInteractionEnabled = true
+            }
+        }
+    }
+
+    /// 現在配列に格納されている(画面上にカードの山として表示されている)Viewの拡大縮小を調節する
+    fileprivate func changeScaleToCardSetViews(skipSelectedView: Bool = false) {
+
+        /// アニメーション関連の定数値
+        let duration: TimeInterval = 0.26
+        let reduceRatio: CGFloat   = 0.1
+
+        var targetCount: CGFloat = 0
+        for (targetIndex, photoCardSetView) in swipePhotoList.enumerated() {
+
+            /// 現在操作中のViewの縮小比を変更しない場合は、以降の処理をスキップする
+            if skipSelectedView && targetIndex == 0 { continue }
+
+            /// 後ろに配置されているViewほど小さく見えるように縮小比を調節する
+            let targetScale: CGFloat = 1 - reduceRatio * targetCount
+            UIView.animate(withDuration: duration, animations: {
+                photoCardSetView.transform = CGAffineTransform(scaleX: targetScale, y: targetScale)
+            })
+            targetCount += 1
+        }
+    }
+    
+    // MARK: - スワイプ後の処理
+    
+    
+    /// 単位回数画像をスワイプしたとき（設定では５回）に次の画像を取得する指示を出す
+    fileprivate func setAdditionPhotoByUnitTimes () {
+        
+        currentLastPhotoNum += 1
+        
+        let addPhotoUnitNum = photoCardSetViewCountLimit/2
+        
+        let addFlg = (currentLastPhotoNum % addPhotoUnitNum == 0)
+
+        if addFlg{
+            /// 次の画像を取得
+            self.addPhotoCardSetViews(bounds: self.setLoadPhotoRange(photoNum: addPhotoUnitNum, isInit: false))
+
+        }
+    }
+    
+    /**
+     画像を取得する際にシングルトンから取得する画像のindexの範囲を決めるメソッド
+     初回読み込み時には、選択画像から数えて10個の画像を取得する
+     :param: photoNum : シングルトンに取得している画像の数
+     :param: isInit : 初回フラグ
+     :returns: 取得する画像のindexの最小値と最大値
+     */
+    private func setLoadPhotoRange(photoNum: Int,isInit : Bool) -> (Int,Int){
+        var minOfRange = 0
+        var maxOfRange = photoCardSetViewCountLimit
+        
+        if isInit {
+            /// 選択されたPhotoにindexが渡されていれば選択したPhotoから表示を開始
+            if let index = self.selectedPhoto?.1  {
+                minOfRange = index
+                currentLastPhotoNum = index
+            }
+            
+            maxOfRange = minOfRange + photoCardSetViewCountLimit
+            
+            if photoNum >= photoCardSetViewCountLimit {
+                /// 一度に追加するのは上限値まで
+                /// デフォルトのまま
+            } else {
+                /// 上限以下の場合には取得枚数分だけ画像を取得する
+                maxOfRange = self.photos!.count
+            }
+        } else {
+            /// 設定値の半分を閲覧したら閲覧した分を追加する
+            minOfRange = currentLastPhotoNum + (photoCardSetViewCountLimit/2)
+            maxOfRange = minOfRange + (photoCardSetViewCountLimit/2)
+        }
+        
+        return (minOfRange,maxOfRange)
+        
+    }
+    
+    /**
+     画像保存する際のアラートを表示
+     OKであれば画像を保存する
+     画像取得後、希望者にはアラートを非表示にする設定を付与
+     :param: targetImage : 保存対象の画像
+     */
+    fileprivate func saveImage (targetImage: UIImage?) {
+        guard let image = targetImage else { return }
+
+        if UserDefaults.standard.object(forKey: "omitConfirmationFlg") == nil {
+            /// 非表示希望者以外（初回のユーザ含む）には保存についての確認をアラートで表示
+            let alertController = UIAlertController(title: "iPhoneへの保存", message: "この画像をiPhoneへ保存しますか？", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .default) { (ok) in
+                /// OKが選択されればアルバムに画像を保存し、アラートの表示設定について確認
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.showConfirmOfOmitAlert(_:didFinishSavingWithError:contextInfo:)), nil)
+            }
+
+            let cancelAction = UIAlertAction(title: "キャンセル", style: .default) { (cancel) in
+                alertController.dismiss(animated: true, completion: nil)
+            }
+            
+            alertController.addAction(cancelAction)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            /// 非表示希望者はアラートなしで画像を保存
+            UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
+        }
+    }
+    
+    /**
+     画像保存実行後のアラートを表示する
+     保存成功した場合、今後の非表示希望についての確認アラートを表示
+     保存失敗時は簡易なアラートを表示
+     */
+    @objc func showConfirmOfOmitAlert(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer) {
+
+        if error == nil {
+            /// 画像の保存に成功
+            let alert = UIAlertController(title: "今後はアラートを非表示にしますか？", message: "非表示にするとよりスムーズに画像の管理ができます", preferredStyle: .alert)
+
+            let okAction = UIAlertAction(title: "OK", style: .default) { (ok) in
+                /// 非表示希望者にはユーザデフォルトでフラグを立てる
+                UserDefaults.standard.set(1, forKey: "omitConfirmationFlg")
+            }
+            
+            let cancelAction = UIAlertAction(title: "キャンセル", style: .default) { (cancel) in
+                alert.dismiss(animated: true, completion: nil)
+            }
+            
+            //OKとCANCELを表示追加し、アラートを表示
+            alert.addAction(cancelAction)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+
+        } else {
+            /// 画像の保存に失敗
+            let alert = UIAlertController(title: "エラー", message: "保存に失敗しました", preferredStyle: .alert)
+            // OKボタンを追加
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            // UIAlertController を表示
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+}
+
+// MARK: - PhotoCardSetDelegate
+
+extension SwipePhotoViewController: SwipePhotoCardViewDelegate {
+    /// ドラッグ処理が開始された際にViewController側で実行する処理
+    func cardViewDidBeginDragging(_ cardView: SwipePhotoCardView) {
+        changeScaleToCardSetViews(skipSelectedView: true)
+    }
+    /// ドラッグ処理中に位置情報が更新された際にViewController側で実行する処理
+    func cardViewDidUpdatePosition(_ cardView: SwipePhotoCardView, centerX: CGFloat, centerY: CGFloat) {
+    }
+    /// 左方向へのスワイプが完了した際にViewController側で実行する処理
+    func cardViewDidSwipeLeftPosition(_ cardView: SwipePhotoCardView) {
+        setAdditionPhotoByUnitTimes()
+        swipePhotoList.removeFirst()
+        enableUserInteractionToFirstCardSetView()
+        changeScaleToCardSetViews(skipSelectedView: false)
+    }
+    /// 右方向へのスワイプが完了した際にViewController側で実行する処理
+    func cardViewDidSwipeRightPosition(_ cardView: SwipePhotoCardView) {
+        saveImage(targetImage: cardView.photoImageView.image)
+        setAdditionPhotoByUnitTimes()
+        swipePhotoList.removeFirst()
+        enableUserInteractionToFirstCardSetView()
+        changeScaleToCardSetViews(skipSelectedView: false)
+    }
+    /// 元の位置へ戻った際にViewController側で実行する処理
+    func cardViewDidReturnToOriginalPosition(_ cardView: SwipePhotoCardView) {
+        changeScaleToCardSetViews(skipSelectedView: false)
+    }
+}
+
+
