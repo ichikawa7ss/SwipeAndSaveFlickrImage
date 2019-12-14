ec5f303200a0168b7c3e90c0d48a046db4abb553
icchii <i2s7.chacha@gmail.com>
2019-12-14 13:55:57 +0900

    画像検索・一覧画面のコンポーネント・レイアウト設定

--- SwipeAndSaveFlickrImage/View/FlickrPhotoCollectionViewController.swift
+++ SwipeAndSaveFlickrImage/View/FlickrPhotoCollectionViewController.swift
@@ -10,39 +10,46 @@ import UIKit
 
 class FlickrPhotoCollectionViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         collectionView.delegate = self
         collectionView.dataSource = self
-
+        searchBar.delegate = self
+        // セルのサイジング
+        setupCell()
+    }
+    
+    func setupCell () {
         //セルの登録
         let nib = UINib(nibName: "FlickrPhotoCollectionViewCell", bundle: Bundle.main)
         collectionView.register(nib, forCellWithReuseIdentifier: "ImageCell")
-
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        let margin: CGFloat = 0.5
+        let length: CGFloat = self.view.frame.width / 3 - 1.5
+        flowLayout.itemSize = CGSize(width: length, height: length)
+        flowLayout.minimumInteritemSpacing = margin
+        flowLayout.minimumLineSpacing = margin
+        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: margin, bottom: margin, right: margin)
+        collectionView.collectionViewLayout = flowLayout
     }
-
-
-
-
-    // MARK: UICollectionViewDelegate
 }
 
+// MARK:  - UICollectionViewDelegate
 extension FlickrPhotoCollectionViewController: UICollectionViewDelegate {
     
 }
 
+// MARK: - UICollectionViewDataSource
 extension FlickrPhotoCollectionViewController: UICollectionViewDataSource {
-    
-    // MARK: UICollectionViewDataSource
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
